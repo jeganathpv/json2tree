8b01dc4c27f62a652d256b7991130f674cb99cd4
revasta <85889757+revasta@users.noreply.github.com>
2021-06-27 17:05:30 +0530

    Added Library into App

--- json2tree-app/src/app/app.module.ts
+++ json2tree-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { Json2TreeModule } from 'json2-tree';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Json2TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
