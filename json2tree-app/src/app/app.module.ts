766f0e897d328f7cea2a64a40bbf02391e846a14
revasta <85889757+revasta@users.noreply.github.com>
2021-06-28 20:21:09 +0530

    Demo component base commit

--- json2tree-app/src/app/app.module.ts
+++ json2tree-app/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+// To use library from public API
+// import { Json2TreeModule } from 'projects/json2-tree/src/public-api';
+
+// To use library pack
 import { Json2TreeModule } from 'json2-tree';
 @NgModule({
   declarations: [
