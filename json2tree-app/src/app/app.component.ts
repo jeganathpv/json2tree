1144663f651abea4b5fe2a5895168da59c9ef8bc
revasta <85889757+revasta@users.noreply.github.com>
2021-07-11 19:42:51 +0530

    [Demo] - Base implementation

--- json2tree-app/src/app/app.component.ts
+++ json2tree-app/src/app/app.component.ts
@@ -6,34 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'json2tree-app';
 
-  data = {
-    "id": "0001",
-    "type": "donut",
-    "name": "Cake",
-    "ppu": 0.55,
-    "batters":
-      {
-        "batter":
-          [
-            { "id": "1001", "type": "Regular" },
-            { "id": "1002", "type": "Chocolate" },
-            { "id": "1003", "type": "Blueberry" },
-            { "id": "1004", "type": "Devil's Food" }
-          ]
-      },
-    "topping":
-      [
-        { "id": "5001", "type": "None" },
-        { "id": "5002", "type": "Glazed" },
-        { "id": "5005", "type": "Sugar" },
-        { "id": "5007", "type": "Powdered Sugar" },
-        { "id": "5006", "type": "Chocolate with Sprinkles" },
-        { "id": "5003", "type": "Chocolate" },
-        { "id": "5004", "type": "Maple" }
-      ]
-  };
+  data =`{\"id\":\"0001\",\"type\":\"donut\",\"name\":\"Cake\",\"ppu\":0.55,\"batters\":{\"batter\":[{\"id\":\"1001\",\"type\":\"Regular\"},{\"id\":\"1002\",\"type\":\"Chocolate\"},{\"id\":\"1003\",\"type\":\"Blueberry\"},{\"id\":\"1004\",\"type\":\"Devil's Food\"}]},\"topping\":[{\"id\":\"5001\",\"type\":\"None\"},{\"id\":\"5002\",\"type\":\"Glazed\"},{\"id\":\"5005\",\"type\":\"Sugar\"},{\"id\":\"5007\",\"type\":\"Powdered Sugar\"},{\"id\":\"5006\",\"type\":\"Chocolate with Sprinkles\"},{\"id\":\"5003\",\"type\":\"Chocolate\"},{\"id\":\"5004\",\"type\":\"Maple\"}]}`;
+
+  text: string = '';
+  // get code () {
+  //   return JSON.stringify(this.data, null, 2);
+  // }
+
+  // set code (v) {
+  //   try{
+  //     this.data = JSON.parse(v);
+  //   }
+  //   catch(e) {
+  //     console.log('error occored while you were typing the JSON');
+  //   };
+  // }
 
   constructor(){
     // this.data = JSON.parse(this.data);
--- json2tree-app/src/app/app.module.ts
+++ json2tree-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 // To use library from public API
@@ -7,12 +8,14 @@ import { AppComponent } from './app.component';
 
 // To use library pack
 import { Json2TreeModule } from 'ngx-json-tree';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     Json2TreeModule
   ],
   providers: [],
